ed9b8f811b7f49dbf45ee0eb6eb0ee6e93a5edfa
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-03-05 15:45:46 +0500

    added navigation till home screen

--- app/src/main/java/com/example/taskaroo/nav_component/AppNavigation.kt
+++ app/src/main/java/com/example/taskaroo/nav_component/AppNavigation.kt
@@ -1,11 +1,21 @@
 package com.example.taskaroo.nav_component
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import com.example.taskaroo.HomeScreen
 import com.example.taskaroo.MainScreen
+import com.example.taskaroo.ProfileScreen
+import com.example.taskaroo.R
+import com.example.taskaroo.SearchScreen
 import com.example.taskaroo.ui.screens.CreateProfile
 import com.example.taskaroo.ui.screens.OnBoardingScreen
 import com.example.taskaroo.ui.screens.SignUpScreen
@@ -27,10 +37,13 @@ sealed class SimpleScreenNavigationItem(val route: String){
     object Signup: SimpleScreenNavigationItem(Screens.SIGNUP.name)
     object UserProfile: SimpleScreenNavigationItem(Screens.USER_PROFILE.name)
     object Main: SimpleScreenNavigationItem(Screens.MAIN.name)
+    object AddTask: SimpleScreenNavigationItem(Screens.ADD_TASK.name)
 }
 
-sealed class BottomNavigationItem(val route: String, val icon: Int, val title: String) {
-
+sealed class BottomNavigationItem(val route: String, val icon: ImageVector, val title: String) {
+    object Home : BottomNavigationItem(Screens.HOME.name, Icons.Rounded.Home, "Home")
+    object Search : BottomNavigationItem(Screens.SEARCH.name, Icons.Rounded.Search, "Search")
+    object Profile : BottomNavigationItem(Screens.PROFILE.name, Icons.Rounded.Person, "Profile")
 }
 //this is out nav_graph
 @Composable
