8f724668efa9cf24ba2ed740fbdc403371472c53
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-03-07 17:50:41 +0500

    completed home screen

---
+++ app/src/main/java/com/example/taskaroo/model/CategoryModel.kt
@@ -0,0 +1,7 @@
+package com.example.taskaroo.model
+
+data class CategoryModel(
+    val icon:Int = 0,
+    val title:String = "",
+    val totalTasks:Int = 0
+)
--- app/src/main/java/com/example/taskaroo/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/taskaroo/ui/screens/MainScreen.kt
@@ -5,20 +5,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -26,80 +33,194 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.taskaroo.R
 import com.example.taskaroo.common.sdp
 import com.example.taskaroo.common.textSdp
+import com.example.taskaroo.model.CategoryModel
 import com.example.taskaroo.ui.theme.backgroundColor
 import com.example.taskaroo.ui.theme.cardColor
-import com.example.taskaroo.ui.theme.darkGray
 import com.example.taskaroo.ui.theme.red
 import com.example.taskaroo.ui.theme.textColor
 
 @Composable
 fun MainScreen() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(backgroundColor)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+    ) {
         //top app bar, name, total tasks and profile picture
         TopBar()
         Spacer(Modifier.height(16.sdp))
+
         SearchBar()
         Spacer(Modifier.height(24.sdp))
+
         SectionCategories()
+        Spacer(Modifier.height(24.sdp))
+
+        SectionTasks()
     }
+}
+
+@Composable
+fun SectionTasks() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 21.sdp)
+    ) {
+        Text(
+            text = "Ongoing Tasks",
+            color = textColor,s
+            fontSize = 18.textSdp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(16.sdp))
+        LazyColumn {
+            items(5) {
+                ItemTaskSection()
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun ItemTaskSection() {
+
+    Card(
+        modifier = Modifier
+            .background(Color.Transparent)
+            .fillMaxWidth()
+            .height(150.sdp)
+            .clickable {
+
+            },
+        shape = RoundedCornerShape(20.sdp),
+        colors = CardDefaults.cardColors(containerColor = cardColor),
+        border = BorderStroke(width = 0.4.dp, color = textColor)
+    ) {
+        Column(modifier = Modifier.padding(16.sdp).fillMaxSize()) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = "Current IDGAF Tasks",
+                    color = textColor,
+                    fontSize = 17.textSdp,
+                    fontWeight = FontWeight.Bold,
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(text = "1 day", color = red, fontSize = 10.textSdp)
+
+            }
+
+            Spacer(modifier = Modifier.height(12.sdp))
+            Text(
+                text = "This task is all about the IDGAF, The IDGAF means not giving fuck about people who are stupid, dont give value to you, to your emotions, time, effort",
+                color = textColor,
+                fontSize = 12.textSdp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+    }
+
 }
 
 @Composable
 fun SectionCategories() {
 
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(horizontal = 21.sdp)) {
+    val categoriesList = listOf(
+        CategoryModel(title = "Mobile App", totalTasks = 10, icon = R.drawable.cat_android),
+        CategoryModel(title = "Web App", totalTasks = 10, icon = R.drawable.cat_web),
+        CategoryModel(title = "Personal ", totalTasks = 10, icon = R.drawable.cat_personal),
+        CategoryModel(title = "Travel", totalTasks = 10, icon = R.drawable.cat_travel),
+        CategoryModel(title = "Professional", totalTasks = 10, icon = R.drawable.cat_work),
+        CategoryModel(title = "Art", totalTasks = 10, icon = R.drawable.car_art),
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 21.sdp)
+    ) {
         Text(
             text = "Categories",
             color = textColor,
             fontSize = 18.textSdp,
             fontWeight = FontWeight.Bold,
         )
+        Spacer(modifier = Modifier.height(16.sdp))
+
+        LazyRow {
+            items(categoriesList) { category ->
+                ItemCategoriesSection(
+                    title = category.title, totalTasks = category.totalTasks, icon = category.icon
+                )
+            }
+        }
+
     }
 
 }
 
 @Composable
-fun ItemCategoriesSection() {
-    Card(modifier = Modifier.size(45.sdp),
-        shape = RoundedCornerShape(12.sdp),
-        colors = CardDefaults.cardColors(containerColor =Color.Transparent),
-        border = BorderStroke(width = 0.4.dp, color = textColor)) {
-        Column {
+fun ItemCategoriesSection(title: String, totalTasks: Int, icon: Int) {
+
+    Box(
+        modifier = Modifier
+            .width(150.sdp)
+            .height(160.sdp)
+            .padding(end = 12.sdp)
+            .clip(RoundedCornerShape(20.sdp))
+            .background(cardColor)
+    ) {
+        Column(
+            modifier = Modifier.padding(start = 16.sdp, top = 16.sdp)
+        ) {
+
             Text(
-                text = "Mobile App",
+                text = title,
                 color = red,
                 fontSize = 17.textSdp,
                 fontWeight = FontWeight.SemiBold,
             )
 
             Text(
-                text = "10 Tasks",
-                modifier = Modifier.padding(top = 3.sdp),
+                text = "${totalTasks} Pending Tasks",
+                modifier = Modifier.padding(top = 2.sdp),
                 color = textColor,
-                fontSize = 14.textSdp,
+                fontSize = 13.textSdp,
                 fontWeight = FontWeight.Normal,
             )
-            Image(
-                painter = painterResource(R.drawable.onboarding_3),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
+
+            Spacer(modifier = Modifier.height(8.sdp))
+
         }
+
+        Image(
+            painter = painterResource(icon),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.sdp)
+                .offset(x = 1.sdp, y = 5.sdp)
+                .align(Alignment.BottomEnd)
+        )
 
     }
 }
@@ -117,14 +238,13 @@ fun SearchBar() {
         colors = CardDefaults.cardColors(containerColor = cardColor),
     ) {
         Row(
-            modifier = Modifier
-                .padding(vertical = 12.sdp, horizontal = 21.sdp),
+            modifier = Modifier.padding(vertical = 12.sdp, horizontal = 21.sdp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
 
             ) {
             Image(
-                imageVector =  Icons.Rounded.Search,
+                imageVector = Icons.Rounded.Search,
                 contentDescription = null,
                 modifier = Modifier
                     .size(25.sdp)
@@ -172,10 +292,12 @@ fun TopBar() {
         }
 
 
-        Card(modifier = Modifier.size(45.sdp),
+        Card(
+            modifier = Modifier.size(45.sdp),
             shape = CircleShape,
-            colors = CardDefaults.cardColors(containerColor =Color.Transparent),
-            border = BorderStroke(width = 0.4.dp, color = textColor)) {
+            colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+            border = BorderStroke(width = 0.4.dp, color = textColor)
+        ) {
             Image(
                 painter = painterResource(R.drawable.onboarding_3),
                 contentDescription = null,
